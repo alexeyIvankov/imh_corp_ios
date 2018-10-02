2cc378977d6aaf9b0ca5b067432dda43c6fbfb51
Alexey Ivankov <alexey.ivankov@biz-tek.ru>
2018-10-02 09:23:31 +0300

    Initial Commit

---
+++ imh_corp_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imh_corp_ios
+//
+//  Created by Alexey Ivankov on 02.10.2018.
+//  Copyright © 2018 Industrial Metallurgical Holding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
