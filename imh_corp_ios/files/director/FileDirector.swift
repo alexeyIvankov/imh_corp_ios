784fab6e040dba1b66d2a31e8ed7a7481252ad19
Alexey Ivankov <alexey.ivankov@biz-tek.ru>
2018-12-19 14:13:05 +0300

    - fixed load file preivew

--- imh_corp_ios/files/director/FileDirector.swift
+++ imh_corp_ios/files/director/FileDirector.swift
@@ -13,15 +13,19 @@ class FileDirector : IFileDirector {
     private let diskService:IFileDiskService
     private let network:INetwork
     private let dataStorage:IFileDataStorage
+    private var queueDownloadOperation:OperationQueue
     
     required init(sessionService:ISessionService,
                   diskService:IFileDiskService,
                   network:INetwork,
                   dataStorage:IFileDataStorage){
+        
         self.sessionService = sessionService
         self.diskService = diskService
         self.network = network
         self.dataStorage = dataStorage
+        
+        self.queueDownloadOperation = OperationQueue()
     }
     
     func giveMeFile(operationId:String,
@@ -92,55 +96,61 @@ class FileDirector : IFileDirector {
                 
                 
                 
-                
-                
-                
             case .newsPreview(let newsId, let fileId): do {
+                
+                DispatchQueue.global().async {
+                  
+
                     let path = self.generatePath(typeFile: typeFile, accountId: accountId)
                     let fileData = self.getFile(path: path)
-                
+                    
                     if fileData != nil{
-                        success(fileData!, operationId)
+                        DispatchQueue.main.async {
+                            success(fileData!, operationId)
+                        }
                     }
                     else {
-                
-                        self.dataStorage.getNewsAttach(accountId: accountId, newsId: newsId, fileId: fileId, completion: { (file) in
-                            
-                            guard file != nil else {
-                                return failed(generateError(message: "file not found"), operationId)
-                            }
-                            
-                            guard file!.previewUrl != nil else {
-                                return failed(generateError(message: "file preview url not found"), operationId)
-                            }
-                            
-                            DispatchQueue.main.async {
-                                 startLoad?()
-                            }
-                            
-                            self.network.apiDirector.socialNetworkModule.image(accessToken: accessToken, networkType: "yammer", imageUrl: file!.previewUrl!, width: 200, height: 200, success: { (data) in
-                                
-                                DispatchQueue.global().async {
-                                    let _ = self.diskService.saveFile(path: path, data: data as NSData)
-                                    
-                                    DispatchQueue.main.async {
-                                        endLoad?()
-                                        success(data, operationId)
-                                    }
-                                }
-                                
-                            }, failed: { (error) in
-                                
-                                DispatchQueue.main.async {
-                                    endLoad?()
-                                    failed(error, operationId)
-                                }
-                            })
-                        })
+                        
+                        self.addLoadAndSavePreviewOperationIfNeed(operation: OperationDownloadAndSavePreview(accountId: accountId,
+                                                                                                             newsId: newsId,
+                                                                                                             fileId: fileId,
+                                                                                                             operationId: operationId,
+                                                                                                             accessToken: accessToken,
+                                                                                                             path: path,
+                                                                                                             sessionService:self.sessionService,
+                                                                                                             diskService: self.diskService,
+                                                                                                             network: self.network,
+                                                                                                             dataStorage: self.dataStorage,
+                                                                                                             success: { (data, operationId) in
+                                                                                                                success(data, operationId)
+                        }, failed: { (error, operationId) in
+                            failed(error, operationId)
+                        }, startLoad: {
+                            startLoad?()
+                        }, endLoad: {
+                            endLoad?()
+                        }, progressLoad: { (progress) in
+                            progressLoad?(progress)
+                        }))
+        
                     }
+                }
                 }
             }
         }
+    }
+    
+    private func addLoadAndSavePreviewOperationIfNeed(operation:OperationDownloadAndSavePreview){
+        
+        let operations = self.queueDownloadOperation.operations as! [OperationDownloadAndSavePreview]
+        
+        for item in operations{
+            
+            if item.fileId == operation.fileId{
+                return
+            }
+        }
+        self.queueDownloadOperation.addOperation(operation)
     }
     
     
@@ -168,5 +178,135 @@ class FileDirector : IFileDirector {
             return nil
         }
         return self.diskService.readFile(path: path)
+    }
+}
+
+class CallBacksStorage
+{
+    private var storageCallbacks:NSMapTable<NSString, AnyObject>
+    private var saveQueue:DispatchQueue
+    
+    required init(){
+        self.storageCallbacks = NSMapTable(keyOptions: NSPointerFunctions.Options.copyIn,
+                                           valueOptions: NSPointerFunctions.Options.strongMemory)
+        self.saveQueue = DispatchQueue(label: "CallBacksStorage.saveQueue")
+    }
+    
+    public func save(id:String, callback:AnyObject){
+        self.saveQueue.async {
+            self.storageCallbacks.setObject(callback, forKey: NSString(string: id))
+        }
+    }
+    
+    public func get(id:String) -> AnyObject?{
+        
+        var callback:AnyObject? = nil
+        
+        self.saveQueue.sync {
+            callback = self.storageCallbacks.object(forKey: NSString(string: id))
+        }
+        
+        return callback
+    }
+}
+
+class OperationDownloadAndSavePreview:Operation{
+    
+    private let sessionService:ISessionService
+    private let diskService:IFileDiskService
+    private let network:INetwork
+    private let dataStorage:IFileDataStorage
+    public  let accountId:String
+    public  let newsId:String
+    public  let fileId:String
+    public  let operationId:String
+    private let accessToken:String
+    private let path:String
+    
+    private let success:(Data, String)->()
+    private let failed: (NSError, String)->()
+    private let startLoad:(()->())?
+    private let endLoad:(()->())?
+    private let progressLoad:((Float)->())?
+    
+    required init(accountId:String,
+                  newsId:String,
+                  fileId:String,
+                  operationId:String,
+                  accessToken:String,
+                  path:String,
+                  sessionService:ISessionService,
+                  diskService:IFileDiskService,
+                  network:INetwork,
+                  dataStorage:IFileDataStorage,
+                  success: @escaping (Data, String)->(),
+                  failed:@escaping  (NSError, String)->(),
+                  startLoad:(()->())?,
+                  endLoad:(()->())?,
+                  progressLoad:((Float)->())?){
+        
+        self.accountId = accountId
+        self.newsId = newsId
+        self.fileId = fileId
+        self.operationId = operationId
+        self.accessToken = accessToken
+        self.path = path
+        
+        self.success = success
+        self.failed = failed
+        self.startLoad = startLoad
+        self.endLoad = endLoad
+        self.progressLoad = progressLoad
+        
+        self.sessionService = sessionService
+        self.diskService = diskService
+        self.network = network
+        self.dataStorage = dataStorage
+    }
+    
+    override func main() {
+        
+        let group = DispatchGroup()
+        group.enter()
+        
+        self.dataStorage.getNewsAttach(accountId: self.accountId, newsId: self.newsId, fileId: self.fileId, completion: { (file) in
+            
+            guard file != nil else {
+                group.leave()
+                return self.failed(generateError(message: "file not found"), self.operationId)
+            }
+            
+            guard file!.previewUrl != nil else {
+                group.leave()
+                return self.failed(generateError(message: "file preview url not found"), self.operationId)
+            }
+            
+            DispatchQueue.main.async {
+                self.startLoad?()
+            }
+            
+            self.network.apiDirector.socialNetworkModule.image(accessToken: self.accessToken, networkType: "yammer", imageUrl: file!.previewUrl!, width: 200, height: 200, success: { (data) in
+                
+                DispatchQueue.global().async {
+                    let _ = self.diskService.saveFile(path: self.path, data: data as NSData)
+                    
+                    DispatchQueue.main.async {
+                        self.endLoad?()
+                        self.success(data, self.operationId)
+                        group.leave()
+                    }
+                }
+                
+            }, failed: { (error) in
+                
+                DispatchQueue.main.async {
+                    self.endLoad?()
+                    self.failed(error, self.operationId)
+                    group.leave()
+                }
+            })
+        })
+        
+        group.wait()
     }
 }
